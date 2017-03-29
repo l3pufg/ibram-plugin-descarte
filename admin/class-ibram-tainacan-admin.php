659d6671922b9773dc9d07ae58fb76090958292d
mbrunodm <mbrunodm@hotmail.com>
2017-03-29 12:59:06 -0300

    Format

--- admin/class-ibram-tainacan-admin.php
+++ admin/class-ibram-tainacan-admin.php
@@ -22,98 +22,96 @@
  */
 class Ibram_Tainacan_Admin {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $plugin_name    The ID of this plugin.
+     */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
-	}
-
-	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/ibram-tainacan-admin.css', array(), $this->version, 'all' );
-	}
-
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ibram-tainacan-admin.js', array( 'jquery' ), $this->version, false );
-	}
-
-	public function add_ibram_tainacan_menu() {
-	    add_options_page('Ibram Plugin', 'IBRAM Config', 'manage_options', $this->plugin_name, array($this, 'display_ibram_setup_page') );
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $plugin_name       The name of this plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct($plugin_name, $version) {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
     }
 
+    /**
+     * Register the stylesheets for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles() {
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/ibram-tainacan-admin.css', array(), $this->version, 'all');
+    }
 
-    public function add_action_links( $links ) {
+    /**
+     * Register the JavaScript for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts() {
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/ibram-tainacan-admin.js', array('jquery'), $this->version, false);
+    }
+
+    public function add_ibram_tainacan_menu() {
+        add_options_page('Ibram Plugin', 'IBRAM Config', 'manage_options', $this->plugin_name, array($this, 'display_ibram_setup_page'));
+    }
+
+    public function add_action_links($links) {
         $settings_link = array(
-            '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+            '<a href="' . admin_url('options-general.php?page=' . $this->plugin_name) . '">' . __('Settings', $this->plugin_name) . '</a>',
         );
-        return array_merge(  $settings_link, $links );
-
+        return array_merge($settings_link, $links);
     }
 
     public function display_ibram_setup_page() {
         include_once( 'partials/class-ibram-tainacan-helper.php' );
         include_once( 'partials/ibram-tainacan-admin-display.php' );
-	}
+    }
 
-	public function validate($input) {
-	    $valid = array();
+    public function validate($input) {
+        $valid = array();
 
-        $valid['comments_css_cleanup'] = (isset($input['comments_css_cleanup']) && !empty($input['comments_css_cleanup'])) ? 1: 0;
-        $valid['bem_permanente'] = (isset( $input['bem_permanente'] ) && ! empty($input['bem_permanente'])) ? $input['bem_permanente'] : 0;
-        $valid['descarte'] = (isset( $input['descarte'] ) && ! empty($input['descarte'])) ? $input['descarte'] : 0;
-        $valid['desaparecimento'] = (isset( $input['desaparecimento'] ) && ! empty($input['desaparecimento'])) ? $input['desaparecimento'] : 0;
+        $valid['comments_css_cleanup'] = (isset($input['comments_css_cleanup']) && !empty($input['comments_css_cleanup'])) ? 1 : 0;
+        $valid['bem_permanente'] = (isset($input['bem_permanente']) && !empty($input['bem_permanente'])) ? $input['bem_permanente'] : 0;
+        $valid['descarte'] = (isset($input['descarte']) && !empty($input['descarte'])) ? $input['descarte'] : 0;
+        $valid['desaparecimento'] = (isset($input['desaparecimento']) && !empty($input['desaparecimento'])) ? $input['desaparecimento'] : 0;
 
-	    return $valid;
+        return $valid;
     }
 
     private function validate_collection_preset($user_sent, $ret_array) {
-	    $cols = ['bem_permanente', 'descarte', 'desaparecimento'];
-	    foreach ($cols as $c => $val) {
-	        if( isset($user_sent[$val]) && !empty($user_sent[$val]) ) {
-	            array_push($ret_array, $user_sent[$val]);
-	        } else {
-	            array_push($ret_array, 0);
-	        }
-	    }
+        $cols = ['bem_permanente', 'descarte', 'desaparecimento'];
+        foreach ($cols as $c => $val) {
+            if (isset($user_sent[$val]) && !empty($user_sent[$val])) {
+                array_push($ret_array, $user_sent[$val]);
+            } else {
+                array_push($ret_array, 0);
+            }
+        }
 
-	    return $ret_array;
-	}
+        return $ret_array;
+    }
 
     public function options_update() {
-	    register_setting($this->plugin_name, $this->plugin_name, array($this, 'validate'));
+        register_setting($this->plugin_name, $this->plugin_name, array($this, 'validate'));
     }
 
 }
