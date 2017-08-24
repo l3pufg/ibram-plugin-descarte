b73947c31678d142a46d6b37e6ad906e72fb94a4
mbrunodm <mbrunodm@hotmail.com>
2017-08-24 16:59:29 -0300

    add data set

---
+++ public/class-helper-file-import-data-set.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: desenvolvedor
+ * Date: 24/08/2017
+ * Time: 16:55
+ */
+
+class HelperFileImportDataSet{
+
+}
---
+++ public/class-import-data-set.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: desenvolvedor
+ * Date: 24/08/2017
+ * Time: 16:41
+ */
+
+class ImportDataSet{
+
+    /**
+     * ImportDataSet constructor.
+     * @param $plugin_name
+     * @param $version
+     */
+    public function __construct() {
+    }
+
+    public function start(){
+
+    }
+
+}
